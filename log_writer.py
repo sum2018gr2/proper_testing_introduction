4b6a0e52ea7a3fb4e1bc4b6bedb20f643404eccc
Alicja <Alicja@MacBook-Air-RG-3.local>
2018-03-23 10:15:08 +0100

    round up to 11 nums

--- log_writer.py
+++ log_writer.py
@@ -132,7 +132,8 @@ class LogWriter(object):
 		# - the value of function computation (in argument)
 		# applied on number 47 
 		# to the output of get_movie_reference
-		return "{}\n{}".format(self.get_movie_reference(), str(computation(47))) \
+		return "{}\n{}".format(self.get_movie_reference(),
+							   round(computation(47),11) if type(computation(47)) is 'float' else computation(47)) \
 			if computation else self.get_movie_reference()
 
 	def combining_method(self):
